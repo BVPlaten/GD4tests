e98be29ad692d05b8e40b2197b10c68af0661907
BVPlaten <bernd.von.platen@gmail.com>
2024-05-06 00:03:02 +0200

    added form selection

--- Rotade2D.gd
+++ Rotade2D.gd
@@ -29,18 +29,33 @@ var figuren = {
     ])
 }
 
+var itemLst 
+
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# polygon.polygon = PackedVector2Array([Vector2(0, 0), Vector2(33, 0), Vector2(33, 33), Vector2(0, 33)])
 	polygon.polygon = figuren["s_form"]
 	polygon.color = Color(1.0, 0.2, 0.2)
 	polygon.position = Vector2(256, 150)
 	polygon.set_antialiased(true)
 	add_child(polygon)
+	itemLst = get_node("ItemList")
+	initLst()
+
 
 
 # rotade the polygon clockwise
 func _process(delta):
 	polygon.rotate(deg_to_rad(delta * 115))
+
+func initLst():
+	for key in figuren:
+		itemLst.add_item(key)
+	itemLst.select(0, true)
+
+
+func _on_item_list_item_clicked(index, at_position, mouse_button_index):
+	print(figuren.keys()[index-1])
+	polygon.polygon = figuren[figuren.keys()[index-1]]
