bef7e80fb1c84ecc5c33c1df441d137c717f4305
BVPlaten <bernd.von.platen@gmail.com>
2024-05-06 20:50:59 +0200

    tetrominos added!

--- Rotade2D.gd
+++ Rotade2D.gd
@@ -1,47 +1,27 @@
 extends Node
 
+var itemSelect      # select-box for the tetromineos
+
 var polygon = Polygon2D.new()
 
-var figuren = {
-    "quadrat": PackedVector2Array([
-        Vector2(0, 0), Vector2(0, 33), Vector2(33, 33), Vector2(33, 0)
-    ]),
-    "dreieck": PackedVector2Array([
-        Vector2(16.5, 0), Vector2(33, 33), Vector2(0, 33)
-    ]),
-    "l_form": PackedVector2Array([
-        Vector2(0, 0), Vector2(33, 0), Vector2(33, 33), Vector2(33, 66), Vector2(0, 66)
-    ]),
-    "umgekehrte_l_form": PackedVector2Array([
-        Vector2(0, 0), Vector2(0, 33), Vector2(0, 66), Vector2(33, 66), Vector2(33, 33), Vector2(66, 33), Vector2(66, 0)
-    ]),
-    "s_form": PackedVector2Array([
-        Vector2(33, 0), Vector2(66, 0), Vector2(66, 33), Vector2(33, 33), Vector2(33, 66), Vector2(0, 66), Vector2(0, 33)
-    ]),
-    "umgekehrte_s_form": PackedVector2Array([
-        Vector2(0, 0), Vector2(33, 0), Vector2(33, 33), Vector2(66, 33), Vector2(66, 66), Vector2(33, 66), Vector2(33, 33)
-    ]),
-    "t_form": PackedVector2Array([
-        Vector2(0, 0), Vector2(33, 0), Vector2(33, 33), Vector2(66, 33), Vector2(66, 0), Vector2(99, 0), Vector2(99, 33), Vector2(66, 33), Vector2(66, 66), Vector2(33, 66), Vector2(33, 33), Vector2(0, 33)
-    ]),
-    "gerade_form": PackedVector2Array([
-        Vector2(0, 0), Vector2(0, 33), Vector2(0, 66), Vector2(0, 99), Vector2(33, 99), Vector2(33, 66), Vector2(33, 33), Vector2(33, 0)
-    ])
-}
-
-var itemLst 
-
-
-
+var tetrominoes = {
+    "o_tetrominoes" : PackedVector2Array([Vector2(0, 0), Vector2(0, 20), Vector2(20, 20), Vector2(20, 0)]),
+    "i_tetrominoes" : PackedVector2Array([Vector2(0, 0), Vector2(0, 10), Vector2(40, 10), Vector2(40, 0)]),
+    "s_tetrominoes" : PackedVector2Array([Vector2(0, 0), Vector2(0, 20), Vector2(10, 20), Vector2(10, 30), Vector2(20, 30), Vector2(20, 10), Vector2(10, 10), Vector2(10, 0)]),
+    "z_tetrominoes" : PackedVector2Array([Vector2(10, 0),Vector2(30, 0), Vector2(30, 10), Vector2(20, 10), Vector2(20, 20), Vector2(0, 20), Vector2(0, 10), Vector2(10, 10)]),
+    "l_tetrominoes" : PackedVector2Array([Vector2(0, 0), Vector2(0, 20), Vector2(10, 20), Vector2(10, 10), Vector2(30, 10), Vector2(30, 0)]),
+    "j_tetrominoes" : PackedVector2Array([Vector2(0, 0), Vector2(0, 20), Vector2(30, 20), Vector2(30, 10), Vector2(10, 10), Vector2(10, 0)]),
+    "t_tetrominoes" : PackedVector2Array([Vector2(10, 0), Vector2(20,0), Vector2(20,10), Vector2(30, 10), Vector2(30, 20), Vector2(0, 20), Vector2(0, 10), Vector2(10, 10)])
+} 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	polygon.polygon = figuren["s_form"]
+	polygon.polygon = tetrominoes["o_tetrominoes"]
 	polygon.color = Color(1.0, 0.2, 0.2)
 	polygon.position = Vector2(256, 150)
 	polygon.set_antialiased(true)
 	add_child(polygon)
-	itemLst = get_node("ItemList")
+	itemSelect = get_node("ItemList")
 	initLst()
 
 
@@ -51,11 +31,11 @@ func _process(delta):
 	polygon.rotate(deg_to_rad(delta * 115))
 
 func initLst():
-	for key in figuren:
-		itemLst.add_item(key)
-	itemLst.select(0, true)
+	for key in tetrominoes:
+		itemSelect.add_item(key)
+	itemSelect.select(0, true)
 
 
 func _on_item_list_item_clicked(index, at_position, mouse_button_index):
-	print(figuren.keys()[index-1])
-	polygon.polygon = figuren[figuren.keys()[index-1]]
+	print(tetrominoes.keys()[index-1])
+	polygon.polygon = tetrominoes[tetrominoes.keys()[index-1]]
